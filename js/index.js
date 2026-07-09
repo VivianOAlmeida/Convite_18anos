64500a7a885ce790426a4bf993b60a8fb2c7c61b
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-07-09 16:10:30 -0300

    corrige erro de sintaxe

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.getElementById('abrir').onclick = () => {
 
   const tipo = document.getElementById('tipo').value;
-      if (tipo === 'familia') location.href = 'convite-familia.html';
-      else location.href = 'convite-amigos.html';
+      if (tipo === 'familia') location.href = 'convite_familia.html';
+      else location.href = 'convite_amigos.html';
 };
